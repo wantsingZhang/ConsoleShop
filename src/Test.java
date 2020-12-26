f5ab6d951dbf6d08b5d50b90726a05e674b72744
wontsing <1187009047@qq.com>
2020-12-26 15:32:51 +0800

    阻塞输入

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("请输入用户名：");
+        Scanner sc=new Scanner (System.in);
+        String username=sc.next();
+        System.out.println("你输入的用户名：" + username);
+        System.out.println("请输入密码：");
+        String password=sc.next();
+        System.out.println("你输入的密码是：" + password);
+
+    }
 }
