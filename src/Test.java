01a156a7a2fa0f197fcf210e0388b651b845cdd7
Wontsing <1187009047@qq.com>
2020-12-26 18:29:02 +0800

    更改Test表格数据

--- src/Test.java
+++ src/Test.java
@@ -10,7 +10,7 @@ public class Test {
         System.out.println("请输入密码：");
         String password=sc.next();
         System.out.println("你输入的密码是：" + password);
-        File file=new File ("C:\\Users\\张万青\\Desktop\\test.xlsx");
+        File file=new File ("D:\\IdeaProjects\\ConsoleShop\\src\\test.xlsx");
         ReadExcel readExcel=new ReadExcel();
         User users[]=readExcel.readExcel(file);
         for (int i=0;i<users.length;i++){
