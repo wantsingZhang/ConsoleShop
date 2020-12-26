6a646a9946a6f808472c711ad44b56e8018a0bd8
wontsing <1187009047@qq.com>
2020-12-26 17:10:00 +0800

    用户类

---
+++ src/User.java
@@ -0,0 +1,39 @@
+public class User {
+    private String Username;
+    private String Password;
+    private String Address;
+    private String Phone;
+
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+}
