8427bdda1216d8918ff42fa399352dd90218e86f
Wontsing <1187009047@qq.com>
2020-12-26 18:22:23 +0800

    读取表格数据，比较数据

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Test {
@@ -9,6 +10,20 @@ public class Test {
         System.out.println("请输入密码：");
         String password=sc.next();
         System.out.println("你输入的密码是：" + password);
+        File file=new File ("C:\\Users\\张万青\\Desktop\\test.xlsx");
+        ReadExcel readExcel=new ReadExcel();
+        User users[]=readExcel.readExcel(file);
+        for (int i=0;i<users.length;i++){
+            if (username.equals(users[i].getUsername())&&password.equals(users[i].getPassword())){
+                System.out.println("登录成功！");
+                break;
+
+            }else {
+                System.out.println("登录失败！");
+            }
+
+        }
+
 
     }
 }
