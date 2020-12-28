3666442ccf52b3315874c603d1ac9325f3bf54a5
Wontsing <1187009047@qq.com>
2020-12-28 18:43:21 +0800

    1、修改从Excel读取数据的方式 2、用户名与密码错误后可以重新输入 3、解决密码与电话号码读取的格式问题 4、新增Product实体类 5、添加读取ProductExcel

---
+++ src/Product.java
@@ -0,0 +1,38 @@
+public class Product {
+    private String pID;
+    private String pName;
+    private String price;
+    private String pDesc;
+
+    public String getpID() {
+        return pID;
+    }
+
+    public void setpID(String pID) {
+        this.pID = pID;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getpDesc() {
+        return pDesc;
+    }
+
+    public void setpDesc(String pDesc) {
+        this.pDesc = pDesc;
+    }
+}
--- src/ReadUserExcel.java
+++ src/ReadUserExcel.java
@@ -7,12 +7,14 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
 
-public class ReadExcel {
-    public User[] readExcel(File file) {
+public class ReadUserExcel {
+    public User[] readExcel(InputStream in) {
         User users[] = null;
         try {
-            XSSFWorkbook xw = new XSSFWorkbook(new FileInputStream(file));
+            XSSFWorkbook xw = new XSSFWorkbook(in);
             XSSFSheet xs = xw.getSheetAt(0);
             users = new User[xs.getLastRowNum()];
             for (int j = 1; j <= xs.getLastRowNum(); j++) {
@@ -34,7 +36,6 @@ public class ReadExcel {
                 }
                 users[j-1] = user;
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,7 +45,7 @@ public class ReadExcel {
     private String getValue(XSSFCell cell) {
         String value;
         CellType type = cell.getCellTypeEnum();
-
+        DecimalFormat df = new DecimalFormat("#");
         switch (type) {
             case STRING:
                 value = cell.getStringCellValue();
@@ -56,7 +57,8 @@ public class ReadExcel {
                 value = cell.getBooleanCellValue() + "";
                 break;
             case NUMERIC:
-                value = cell.getNumericCellValue() + "";
+                value = df.format(cell.getNumericCellValue());//double和一个空字符串相连接，最终得到字符串
+
                 break;
             case FORMULA:
                 value = cell.getCellFormula();
